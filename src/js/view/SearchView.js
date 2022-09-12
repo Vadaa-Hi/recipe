3b0d2a4f342f18a317d02a5ec33e8331d9705c43
Vadaa-Hi <69497241+Vadaa-Hi@users.noreply.github.com>
2022-09-12 15:50:06 +0800

    pagination

--- src/js/view/SearchView.js
+++ src/js/view/SearchView.js
@@ -23,8 +23,44 @@ export const clearSearchQuery = () => {
 };
 export const clearSearchResult = () => {
   elements.searchResultList.innerHTML = '';
+  elements.pageButtons.innerHTML = '';
 };
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = (recipes) => {
-  recipes.forEach(renderRecipe);
+export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
+  // Хайлтын үр дүнг хуудаслаж үзүүлэх
+  // page = 2, start = 10, end = 20
+  const start = (currentPage - 1) * resPerPage;
+  const end = currentPage * resPerPage;
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  //хуудаслалтын товчуудыг гаргах
+  const totalPages = Math.ceil(recipes.length / resPerPage);
+  renderButttons(currentPage, totalPages);
+};
+// type ===> 'prev' , 'next'
+const createButton = (
+  page,
+  type,
+  direction
+) => `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+<span>Хуудас ${page}</span>
+  <svg class="search__icon">
+      <use href="img/icons.svg#icon-triangle-${direction}"></use>
+  </svg>
+  </button>`;
+
+const renderButttons = (currentPage, totalPages) => {
+  let buttonHtml;
+  if (currentPage === 1 && totalPages > 1) {
+    //1-r huudsan dr baina, 2-r huudas gdg tovchiig garga
+    buttonHtml = createButton(2, 'next', 'right');
+  } else if (currentPage < totalPages) {
+    // omnoh bln daraachiiin huudas ruu shiljih tovchiig uzuul
+    buttonHtml = createButton(currentPage - 1, 'prev', 'left');
+    buttonHtml += createButton(currentPage + 1, 'next', 'right');
+  } else if (currentPage === totalPages) {
+    // Hamgiin suuliin huudas dr baina. Omnoh ruu shiljuuleh tovchiig l uzuulne.
+    buttonHtml = createButton(currentPage - 1, ' prev', 'left');
+  }
+  elements.pageButtons.insertAdjacentHTML('afterbegin', buttonHtml);
 };
